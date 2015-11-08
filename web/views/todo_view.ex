576a028a728883f13311f5970dc4d83bd9f7e4fd
Manu S Ajith <neo@codingarena.in>
2015-11-08 06:28:04 +0530

    Update root key to todo/todos
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- web/views/todo_view.ex
+++ web/views/todo_view.ex
@@ -2,11 +2,11 @@ defmodule PhoenixTodoApi.TodoView do
   use PhoenixTodoApi.Web, :view
 
   def render("index.json", %{todos: todos}) do
-    %{data: render_many(todos, PhoenixTodoApi.TodoView, "todo.json")}
+    %{todos: render_many(todos, PhoenixTodoApi.TodoView, "todo.json")}
   end
 
   def render("show.json", %{todo: todo}) do
-    %{data: render_one(todo, PhoenixTodoApi.TodoView, "todo.json")}
+    %{todo: render_one(todo, PhoenixTodoApi.TodoView, "todo.json")}
   end
 
   def render("todo.json", %{todo: todo}) do
